66ca85f9374b9acb1a34d7a39d82933d17641272
Victor <VictorCarmona9035@hotmail.com>
2023-10-03 19:56:15 +0200

    36-Como acceder a los valores de un objeto

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,17 @@
+//Object literal
+const producto = {
+    nombre: "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible: true
+  }
+  
+  
+  console.log(producto);
+
+  //Forma 1
+  console.log(producto.nombre);
+
+  //Forma 2
+  console.log(producto['nombre'])
+
+  
